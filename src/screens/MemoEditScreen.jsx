8483881ae5f84008292252dd4b4475b011dde4bd
ryochin3 <ryota.ozkai51@gmail.com>
2023-06-06 17:40:56 +0900

    Create MemoEditScreen and MemoCreateScreen

---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  View, TextInput, StyleSheet, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleBtn from '../components/CircleBtn';
+
+export default function MemoEditScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="買い物リスト" multiline style={styles.input} />
+      </View>
+      <CircleBtn name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+    flex: 1,
+  },
+  input: {
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+    flex: 1,
+  },
+});
